31e217288c5692a2bb7ec17635c372e320916726
Jackson Ming Hu <huming2207@gmail.com>
2020-05-06 21:58:19 +1000

    Home: apply linter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,12 +8,12 @@
 
 <script lang="ts">
 // @ is an alias to /src
-import Login from '@/components/Login.vue';
+import Login from "@/components/Login.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    Login,
-  },
+    Login
+  }
 };
 </script>
