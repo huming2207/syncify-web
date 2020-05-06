a7308a1f574b7671d6cfe7e2e75b43c48c287821
Jackson Ming Hu <huming2207@gmail.com>
2020-05-06 21:49:28 +1000

    Implement login page

---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="login">
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4">
+          <v-card class="elevation-12">
+            <v-toolbar color="primary" dark flat>
+              <v-toolbar-title>Login</v-toolbar-title>
+              <v-spacer />
+            </v-toolbar>
+            <v-card-text>
+              <v-form>
+                <v-text-field label="Login" name="login" prepend-icon="mdi-account" type="text" />
+
+                <v-text-field
+                  id="password"
+                  label="Password"
+                  name="password"
+                  type="password"
+                  prepend-icon="mdi-key"
+                />
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="primary">Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Login',
+});
+</script>
