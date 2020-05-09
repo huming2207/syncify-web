9e7dedf1818caf796a02afa824985f15f4526084
Jackson Ming Hu <huming2207@gmail.com>
2020-05-09 17:55:47 +1000

    component: file browser: implement file uploader

--- src/components/FileBrowser.vue
+++ src/components/FileBrowser.vue
@@ -7,9 +7,67 @@
         :items-per-page="10"
         class="elevation-1"
         :key="tableRenderCounter"
-        @load="loadTable"
         @click:row="handleItemClick"
       >
+        <template v-slot:top>
+          <v-toolbar flat color="white">
+            <v-toolbar-title>File browser</v-toolbar-title>
+            <v-divider class="mx-4" inset vertical></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog v-model="uploadDialog" max-width="500px">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark class="mb-2" v-on="on">
+                  <v-icon left>mdi-cloud-upload</v-icon> Upload files
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">Upload new file</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <vue-dropzone
+                      id="dropzone"
+                      ref="dropzone"
+                      @vdropzone-file-added="updateDropzoneUrl()"
+                      :options="dropzoneOptions"
+                    ></vue-dropzone>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="finishFileUpload()">Done</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="newDirDialog" max-width="500px">
+              <template v-slot:activator="{ on }">
+                <v-btn color="success" dark class="mb-2 ml-2" v-on="on">
+                  <v-icon left>mdi-folder-plus</v-icon> New directory
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="headline">Create new directory</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-text-field
+                      v-model="newDirName"
+                      label="New directory name"
+                      required
+                    ></v-text-field>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="newDirDialog = false">Cancel</v-btn>
+                  <v-btn color="blue darken-1" text @click="createNewDir()">Create</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-toolbar>
+        </template>
         <template v-slot:item.actions="{ item }">
           <v-icon medium class="mr-4" @click="editItem(item)">
             mdi-pencil
@@ -27,13 +85,13 @@
           </v-icon>
         </template>
       </v-data-table>
-      <v-dialog v-model="dialog" width="500">
+      <v-dialog v-model="errorDialog" width="500">
         <v-card>
           <v-card-title class="headline">
-            {{ dialogTitle }}
+            {{ errorDialogTitle }}
           </v-card-title>
           <v-card-text>
-            {{ dialogText }}
+            {{ errorDialogText }}
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
@@ -50,9 +108,14 @@
 <script>
 import axios from "axios";
 import path from "path";
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
 import { loginWithJwt } from "../common/AxiosHelper";
 
 export default {
+  components: {
+    vueDropzone: vue2Dropzone
+  },
   data: () => ({
     headers: [
       { text: "", value: "typeIcon", sortable: false },
@@ -61,12 +124,27 @@ export default {
       { text: "Size", value: "size", sortable: true },
       { text: "Actions", value: "actions", sortable: false }
     ],
-    files: [{ type: "Directory", size: 0, name: ".." }], // Default ".." (parent) directory
-    dialog: false,
+    files: [],
+    errorDialog: false,
     unauthorised: false,
-    dialogTitle: "",
-    dialogText: "",
-    tableRenderCounter: 0
+    errorDialogTitle: "",
+    errorDialogText: "",
+    tableRenderCounter: 0,
+    uploadDialog: false,
+    newDirDialog: false,
+    chosenFile: null,
+    newDirName: "",
+    dropzoneOptions: {
+      url: "/api/file",
+      thumbnailWidth: 100,
+      headers: { ...loginWithJwt().headers },
+      dictDefaultMessage: "Pick a new file",
+      maxFiles: 1,
+      maxfilesexceeded(file) {
+        this.removeAllFiles();
+        this.addFile(file);
+      }
+    }
   }),
   props: {
     path: String
@@ -76,6 +154,8 @@ export default {
   },
   methods: {
     async loadTable() {
+      this.files = []; // Clear the file object list
+      this.files.push({ type: "Directory", size: 0, name: ".." }); // Add the ".." (to parent directory)
       const currPath = this.path || "/";
       try {
         const resp = await axios.get("/api/path", {
@@ -87,9 +167,9 @@ export default {
 
         console.log(resp);
         if (!resp.data.data) {
-          this.dialogText = "Please check your internet connection";
-          this.dialogTitle = "Something went wrong...";
-          this.dialog = true;
+          this.errorDialogText = "Please check your internet connection";
+          this.errorDialogTitle = "Something went wrong...";
+          this.errorDialog = true;
         }
 
         const { data } = resp.data;
@@ -103,20 +183,20 @@ export default {
       } catch (err) {
         if (err && err.response) {
           const resp = err.response;
-          this.dialogText = resp.data.message;
-          this.dialogTitle = "Something went wrong...";
-          this.dialog = true;
+          this.errorDialogText = resp.data.message;
+          this.errorDialogTitle = "Something went wrong...";
+          this.errorDialog = true;
           this.unauthorised = resp.status === 401;
         } else {
           console.log(err);
-          this.dialogText = "Unknown error, please check your Internet connection";
-          this.dialogTitle = "Something went wrong...";
-          this.dialog = true;
+          this.errorDialogText = "Unknown error, please check your Internet connection";
+          this.errorDialogTitle = "Something went wrong...";
+          this.errorDialog = true;
         }
       }
     },
     closeDialog() {
-      this.dialog = false;
+      this.errorDialog = false;
       if (this.unauthorised) this.$router.push({ path: "/" });
     },
     handleItemClick(item) {
@@ -131,6 +211,19 @@ export default {
             return true; // Suppress errors here (useless)
           });
       }
+    },
+    updateDropzoneUrl() {
+      console.log(this.$refs.dropzone);
+      this.$refs.dropzone.setOption("url", `/api/file?path=${this.path}`);
+    },
+    async finishFileUpload() {
+      await this.loadTable();
+      this.$refs.dropzone.removeAllFiles();
+      this.uploadDialog = false;
+      this.tableRenderCounter += 1;
+    },
+    createNewDir() {
+      console.log(this.chosenFile);
     }
     // editItem(item) {},
     // copyItem(item) {},
