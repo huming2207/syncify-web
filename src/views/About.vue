42ec07329198ba0a0c2da11d93e736df1bd7ac7b
Jackson Ming Hu <huming2207@gmail.com>
2020-05-12 22:52:43 +1000

    Add about info

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-content>
+      <v-container>
+        <h1>About this project</h1>
+        <p>
+          <span><v-icon>mdi-account</v-icon></span>
+          Written by Jackson Hu
+        </p>
+        <p>
+          <span><v-icon>mdi-github</v-icon></span>
+          GitHub: <a href="https://github.com/huming2207/syncify">huming2207/syncify</a>
+        </p>
+        <p>
+          <span><v-icon>mdi-license</v-icon></span>
+          License: SSPL (like MongoDB)
+        </p>
+      </v-container>
+    </v-content>
   </div>
 </template>
